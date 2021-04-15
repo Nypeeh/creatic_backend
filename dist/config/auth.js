2a6eeea21d5861be52299a79673c77eb4e2fff2c
nypeeh <lammerlammer@outlook.com>
2021-04-15 11:55:47 -0300

    Fix cors and start

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '2ee3215c8f7ba0aa7c5551257801245f',
+        expiresIn: '1d',
+    },
+};
