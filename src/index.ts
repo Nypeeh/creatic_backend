2906e033c107d939dd9005a8988335d0aaeb76a2
nypeeh <lammerlammer@outlook.com>
2021-04-22 18:46:01 -0300

    Config port

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ app.use(cors())
 
 app.use(routes)
 
-app.listen(3333, () => {
-  console.log(`😊 Server started on port 3333!`)
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`😊 Server started on port ${process.env.PORT || 3333}!`)
 })
