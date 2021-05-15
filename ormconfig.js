5da89fcdb48ccbb57cc12fcea0c6f1bd68086a2b
Leeo <leeo@pop-os.localdomain>
2021-05-15 08:34:29 -0300

    conf

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,11 @@
 module.exports = {
+  "url": "postgres://eoteltogucmwtx:8d85ad6167ef3c23348ed20384a99f049d9fe2faf16a01bb27cce799a7829de8@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d5sj5ks3pca70j",
   type: "postgres",
-  "url": process.env.DATABASE_URL,
-
+  "host": "ec2-107-20-153-39.compute-1.amazonaws.com",
+  "port": 5432,
+  "username": "eoteltogucmwtx",
+  "password": "8d85ad6167ef3c23348ed20384a99f049d9fe2faf16a01bb27cce799a7829de8",
+  database: "d5sj5ks3pca70j",
   entities: [
   "dist/src/models/*.js"
   ],
